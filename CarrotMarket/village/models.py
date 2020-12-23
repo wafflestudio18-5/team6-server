d288ecae9199bc34b5cabf4f12c31b941a3a2482
Unamq <69029281+Noaarhk@users.noreply.github.com>
2020-12-23 22:36:36 +0900

    Model design (#1)
    * Specify Models
    
    * Modify model
    
    * Modify model

--- CarrotMarket/village/models.py
+++ CarrotMarket/village/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+<<<<<<< HEAD
 
 class CategoryOfArticle(models.Model):
     CATEGORY_ARTICLE = (
@@ -51,3 +52,44 @@ class LikeArticle(models.Model):
         unique_together = (
             ('user', 'article')
         )
+||||||| parent of 4c50bfc (Model design (#1))
+# Create your models here.
+=======
+
+class CategoryOfArticle(models.Model):
+    CATEGORY_ARTICLE = (
+        (1, '동네사건사고'),
+        (2, '동네생활이야기'),
+        (3, '분실/실종센터'),
+        (4, '우리동네질'),
+    )
+
+    category_article = models.PositiveSmallIntegerField(primary_key=True, choices=CATEGORY_ARTICLE)
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, related_name='article', on_delete=models.CASCADE)
+    category = models.ForeignKey(CategoryOfArticle, related_name='article', on_delete=models.CASCADE)
+    content = models.TextField(db_index=True)
+    title = models.CharField(max_length=50, db_index=True)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, related_name='comment', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='comment', on_delete=models.CASCADE)
+    content = models.CharField(max_length=100, db_index=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+
+class LikeArticle(models.Model):
+    user = models.ForeignKey(User, related_name='like_article', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, related_name='like_article', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (
+            ('user', 'article')
+        )
+>>>>>>> 4c50bfc (Model design (#1))
