edcc1089760547d1e68415306f80dde9be4b2b53
kmy17518 <69717995+kmy17518@users.noreply.github.com>
2020-12-23 22:25:16 +0900

    create views.py serializers.py

---
+++ CarrotMarket/user/serializers.py
@@ -0,0 +1,88 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.db import transaction
+from rest_framework import serializers, status
+from rest_framework.authtoken.models import Token
+
+from user.models import UserProfile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(allow_blank=False)
+    password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    last_login = serializers.DateTimeField(read_only=True)
+    joined_at = serializers.DateTimeField(read_only=True)
+    profile = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'last_login',
+            'joined_at',
+            'profile',
+        )
+
+    def get_profile(self, user):
+        return UserProfileSerializer(user.profile, context=self.context).data
+
+    def validate_password(self, value):
+        return make_password(value)
+
+    def validate(self, data):
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        if bool(first_name) ^ bool(last_name):
+            api_exception = serializers.ValidationError("First name and last name should appear together.")
+            api_exception.status_code = status.HTTP_400_BAD_REQUEST
+            raise api_exception
+        if first_name and last_name and not (first_name.isalpha() and last_name.isalpha()):
+            api_exception = serializers.ValidationError("First name or last name should not have number.")
+            api_exception.status_code = status.HTTP_404_BAD_REQUEST
+            raise api_exception
+
+
+        profile_serializer = UserProfileSerializer(data=data, context=self.context)
+        profile_serializer.is_valid(raise_exception=True)
+        return data
+
+    @transaction.atomic
+    def create(self, validated_data):
+        area = validated_data.pop('area')
+        kindness = validated_data.pop('kindness', None)
+        nickname = validated_data.pop('nickname', '')
+        phone = validated_data.pop('phone', '')
+
+        user = super(UserSerializer, self).create(validated_data)
+        Token.objects.create(user=user)
+
+        UserProfile.objects.create(user=user, area=area, kindness=kindness, nickname=nickname, phone=phone)
+
+        return user
+
+    def update(self, user, validated_data):
+        user_profile = user.profile
+        area = validated_data.get('area')
+        kindness = validated_data.get('kindness')
+        nickname = validated_data.get('nickname')
+        phone = validated_data.get('phone')
+        if area is not None or nickname is not None or phone is not None or kindness:
+            if area is not None:
+                user_profile.area = area
+            if nickname is not None:
+                user_profile.nickname = nickname
+            if phone is not None:
+                user_profile.phone = phone
+            if kindness:
+                user_profile.kindness = kindness
+            user_profile.save()
+        super(UserSerializer, self).update(user, validated_data)
+
--- CarrotMarket/user/views.py
+++ CarrotMarket/user/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from rest_framework import status, viewsets
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from user.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_permissions(self):
+        if self.action in ('create', 'login'):
+            return (AllowAny(), )
+        return super(UserViewSet, self).get_permissions()
+
+    # POST /api/v1/user/ 회원가입
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            user = serializer.save()
+        except IntegrityError:
+            return Response({"error": "A user with that username already exists."}, status=status.HTTP_400_BAD_REQUEST)
+
+        login(request, user)
+
+        data = serializer.data
+        data['token'] = user.auth_token.key
+        return Response(data, status=status.HTTP_201_CREATED)
+
+    # PUT /api/v1/user/login/  로그인
+    @action(detail=False, methods=['PUT'])
+    def login(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+
+            data = self.get_serializer(user).data
+            token, created = Token.objects.get_or_create(user=user)
+            data['token'] = token.key
+            return Response(data)
+
+        return Response({"error": "Wrong username or wrong password"}, status=status.HTTP_403_FORBIDDEN)
+
+    @action(detail=False, methods=['POST']) #로그아웃
+    def logout(self, request):
+        logout(request)
+        return Response()
+
+    # Get /api/v1/user/{user_id} # 유저 정보 가져오기(나 & 남)
+    def retrieve(self, request, pk=None):
+        if pk =='me':
+            user = request.user
+        else:
+            try:
+                user = User.objects.get(pk=pk)
+            except User.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(self.get_serializer(user).data, status=status.HTTP_200_OK)
+
+    # PUT /api/v1/user/me/  # 유저 정보 수정 (나)
+    def update(self, request, pk=None):
+        if pk != 'me':
+            return Response({"error": "Can't update other Users information"}, status=status.HTTP_403_FORBIDDEN)
+
+        user = request.user
+
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(user, serializer.validated_data)
+        return Response(serializer.data)X
