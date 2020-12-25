dc5c74cf1ef1cae25e433c7f9cd09d65f8482d14
Noaarhk <skarb8277@naver.com>
2020-12-25 19:54:53 +0900

    Add Comment & Like

---
+++ CarrotMarket/village/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from village.views import ArticleViewSet, CommentViewSet
+
+app_name = 'village'
+
+router = SimpleRouter()
+router.register('feed', ArticleViewSet, basename='feed')
+router.register('comment', CommentViewSet, basename='comment')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
