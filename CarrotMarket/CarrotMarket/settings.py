e4b93c79b4290a1113d1aff40f8cfaa24f677646
Noaarhk <skarb8277@naver.com>
2020-12-28 10:25:58 +0900

    IDK

--- CarrotMarket/CarrotMarket/settings.py
+++ CarrotMarket/CarrotMarket/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'user.apps.UserConfig',
+
     'product',
     'village',
 ]
@@ -124,3 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    )
+
+}
